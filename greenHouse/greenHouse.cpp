41e88039761a49585ef36ef7f88be4d72a70767c
Holyfrog115 <149291415+Holyfrog115@users.noreply.github.com>
2026-06-04 11:41:49 +0700

    Add project files.

---
+++ greenHouse/greenHouse.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+
+enum healthStatus {
+	HEALTHY,
+	DRY,
+	DEAD,
+};
+
+
+class Plant {
+private:
+	std::string plantName;
+	int currentHumidity;
+	healthStatus health;
+public:
+	Plant(std::string plantName = "Sunflower", int currentHumidity = 100, healthStatus health = HEALTHY) {
+		this->plantName = plantName;
+		this->currentHumidity = currentHumidity;
+		this->health = health;
+	}
+
+	void water() {
+		if (currentHumidity + 20 > 100) {
+			currentHumidity = 100;
+		}
+		else {
+			currentHumidity += 20;
+		}
+	}
+
+	void updateStatus() {
+		int decrease = rand() % 25 + 5;
+		currentHumidity -= decrease;
+	}
+};
+
+
+class greenHouseController {
+private:
+	Plant plants[10];
+public:
+	void checkAllPlants() {
+		for (Plant plant : plants) {
+
+		}
+	}
+};
+
+
+int main() {
+
+}
